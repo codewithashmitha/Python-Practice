895f8deb4b697a3f73f24618bda99d5312e41d23
Ramidi Ashmitha Reddy <ashmithareddy3456@gmail.com>
2026-06-12 20:11:57 +0530

    Add files via upload

---
+++ areavolcalci.py
@@ -0,0 +1,7 @@
+length = float(input("enter length of the rectangle:"))
+width = float(input("enter width of the rectangle:"))
+height = float(input("enter height of the rectangle:"))
+area = length * width
+vol = length * width * height
+print(f" The area of rectangle is: {area}")
+print(f" The volume of rectangle is: {vol}")
