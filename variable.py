ff519102676895049a902e5c04cd6ca8a1544bbc
Ramidi Ashmitha Reddy <ashmithareddy3456@gmail.com>
2026-06-12 17:58:55 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,8 @@
+name = "Ashmitha"
+print(f"1-who are you?\n   My self {name}.")     #STRING
+age = 19
+print(f"2-How old are you?\n  {age} years old.") # INTEGER
+cgpa = 8.3
+print(f"3-CGPA?\n  {cgpa} of two years.")        # FLOAT
+github = True
+print(f"4-Using GITHUB?\n  {github}.")           # BOOLEAN 
