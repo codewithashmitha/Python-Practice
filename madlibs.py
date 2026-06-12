b420acd792520eb1e3b379020aca2921eafaaf4f
Ramidi Ashmitha Reddy <ashmithareddy3456@gmail.com>
2026-06-12 19:59:12 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+adjective = input("enter the adjective: ")
+noun = input("enter the noun: ")
+verb = input("enter the verb: ")
+
+print(f"I went to a {adjective} place")
+print(f"where I saw {noun}")
+print(f"the {noun} is {verb}ing")
